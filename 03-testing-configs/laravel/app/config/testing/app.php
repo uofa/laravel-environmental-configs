20d3be479d6305f1ad8300f3061eca6d93740e65
Jonathan Goode <u01jmg3@gmail.com>
2016-04-01 19:09:53 +0100

    Rename `test` to `testing`

---
+++ 03-testing-configs/laravel/app/config/testing/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'debug' => false,
+    'timezone' => 'Europe/London',
+    'cron_token' => '<replace-with-random-hash>',
+    'test_email' => '<replace-with-test-email>@abdn.ac.uk', // Not used in this environment but must be defined
+    'url' => 'https://homepages.abdn.ac.uk/xxx999/',
+);
---
+++ 03-testing-configs/laravel/bootstrap/environment.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Environment variable
+|--------------------------------------------------------------------------
+|
+| This variable is responsible for setting the environment without
+| requiring a hostname. This file should not be committed to the repo.
+|
+|
+|
+*/
+
+return 'testing';
